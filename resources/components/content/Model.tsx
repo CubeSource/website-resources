4c1b69154e400c1fe3a993cb26a9e7a8e8c72a88
CubeSource <info@cubesource.space>
2025-11-26 21:03:33 -0700

    Fix model source path in ModelViewer component

--- resources/components/content/Model.tsx
+++ resources/components/content/Model.tsx
@@ -59,7 +59,7 @@ function EnsureDoubleSided({ scene }: { scene: any }) {
 
 function ModelViewer({ src }: { src: string }) {
   
-  const { scene: originalScene } = useGLTF(`/models/${src}`);
+  const { scene: originalScene } = useGLTF(src);
   const [processedScene, setProcessedScene] = useState<any>(null);
   const meshRef = useRef<any>(null);
 
